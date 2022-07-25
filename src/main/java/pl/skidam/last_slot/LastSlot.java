b432836e84d7c9c230c7d8f9f98722c16c82f9a6
Skidam <skidamgamer@gmail.com>
2022-07-25 13:47:11 +0200

    mod from template

---
+++ src/main/java/pl/skidam/last_slot/LastSlot.java
@@ -0,0 +1,15 @@
+package pl.skidam.last_slot;
+
+import net.fabricmc.api.ModInitializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class LastSlot implements ModInitializer {
+	public static final String MOD_ID = "Last slot";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+
+	}
+}
